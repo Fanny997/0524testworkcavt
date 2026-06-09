42f4cf25433ca7151a34790d87c2109c30a2b4c5
Fanny997 <2826878647@qq.com>
2026-06-10 01:32:46 +0800

    Delete node_modules directory

--- node_modules/cvat-ui/node_modules/cvat-canvas3d/src/typescript/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (C) CVAT.ai Corporation
-//
-// SPDX-License-Identifier: MIT
-
-export { default as ObjectState } from 'cvat-core/src/object-state';
-export { ObjectType } from 'cvat-core/src/enums';
